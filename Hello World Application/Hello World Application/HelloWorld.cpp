bd05b7d9856928ac47aba584ed0e3e4a0bb78e8e
Kevin Day <kevin.ch.day@gmail.com>
2022-03-06 12:00:30 -0600

    Initial Commit

---
+++ Hello World Application/Hello World Application/HelloWorld.cpp
@@ -0,0 +1,16 @@
+/* HelloWorld.cpp
+ * Hello World using C++ 
+ */
+
+#include <iostream>
+
+using namespace std;
+
+// main()
+int main() {
+	system("color 0a");
+	cout << "Hello World\n";
+	system("pause");
+
+	return 0;
+}
